5b618a52bc0fcf88d33af12edf5b4fc3a764c655
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-07-15 17:58:49 +0800

    done fix

--- app/Http/Controllers/RawMaterialController.php
+++ app/Http/Controllers/RawMaterialController.php
@@ -10,47 +10,54 @@ class RawMaterialController extends Controller
     // List
     public function index(Request $request)
     {   
-        if(request()->ajax())
-        {
-            $search = $request->input('search.value');
+        // if(request()->ajax())
+        // {
+        //     $search = $request->input('search.value');
 
-            if ($search != null)
-            {
-                RawMaterial::where('Name', 'LIKE', '%'.$search.'%')->orWhere('Description', 'LIKE', '%'.$search.'%');
-            }
+        //     if ($search != null)
+        //     {
+        //         RawMaterial::where('Name', 'LIKE', '%'.$search.'%')->orWhere('Description', 'LIKE', '%'.$search.'%');
+        //     }
             
-            return datatables()->of(RawMaterial::orderBy('id', 'desc')->get())
-                    ->addColumn('action', function($data){
-                        $buttons = '
-                            <button type="button" name="view" class="view btn-table btn btn-success viewModal" title="View" data-id="'.$data->id.'">
-                                <i class="ti-eye"></i>
-                            </button>
-                        ';
+        //     return datatables()->of(RawMaterial::orderBy('id', 'desc')->get())
+        //             ->addColumn('action', function($data){
+        //                 $buttons = '
+        //                     <button type="button" name="view" class="view btn-table btn btn-success viewModal" title="View" data-id="'.$data->id.'">
+        //                         <i class="ti-eye"></i>
+        //                     </button>
+        //                 ';
 
-                        if ($data->status == "Active")
-                        {
-                            $buttons.= '
-                                <button type="button" class="delete btn-table btn btn-danger deactivate" title="Deactivate" data-id="'.$data->id.'">
-                                    <i class="ti-trash"></i>
-                                </button>
-                            ';
-                        }
-                        else
-                        {
-                            $buttons.= '
-                                <button type="button" class="btn btn-sm btn-info activate" title="Activate" data-id="'.$data->id.'">
-                                    <i class="ti-check"></i>
-                                </button>
-                            ';
-                        }
+        //                 if ($data->status == "Active")
+        //                 {
+        //                     $buttons.= '
+        //                         <button type="button" class="delete btn-table btn btn-danger deactivate" title="Deactivate" data-id="'.$data->id.'">
+        //                             <i class="ti-trash"></i>
+        //                         </button>
+        //                     ';
+        //                 }
+        //                 else
+        //                 {
+        //                     $buttons.= '
+        //                         <button type="button" class="btn btn-sm btn-info activate" title="Activate" data-id="'.$data->id.'">
+        //                             <i class="ti-check"></i>
+        //                         </button>
+        //                     ';
+        //                 }
 
-                        return $buttons;
-                    })
-                    ->rawColumns(['action'])
-                    ->make(true);
-        }
+        //                 return $buttons;
+        //             })
+        //             ->rawColumns(['action'])
+        //             ->make(true);
+        // }
+
+        $rawMaterials = RawMaterial::where('Name', 'LIKE', '%'.$request->search.'%')->orWhere('Description', "LIKE", "%".$request->search."%")->orderBy('id', 'desc')->paginate(10);
         
-        return view('raw_materials.index'); 
+        return view('raw_materials.index',
+            array(
+                'search' => $request->search,
+                'rawMaterials' => $rawMaterials
+            )
+        ); 
     }
 
     public function add(Request $request)
@@ -61,10 +68,8 @@ class RawMaterialController extends Controller
         $rawMaterial->status = "Active";
         $rawMaterial->save();
 
-        return response()->json([
-            'message' => 'Successfully Saved.',
-            'status' => 1
-        ]);
+        Alert::success('Sucessfully Saved')->persistent('Dismiss');
+        return back();
     }
 
     public function deactivate(Request $request)
@@ -73,8 +78,8 @@ class RawMaterialController extends Controller
         $rawMaterial->status = "Inactive";
         $rawMaterial->save();
         
-        // Alert::success('Sucessfully Deactivate')->persistent('Dismiss');
-        // return back();
+        Alert::success('Sucessfully Deactivate')->persistent('Dismiss');
+        return back();
     }
 
     public function activate(Request $request)
@@ -83,21 +88,21 @@ class RawMaterialController extends Controller
         $rawMaterial->status = "Active";
         $rawMaterial->save();
         
-        // Alert::success('Sucessfully Activate')->persistent('Dismiss');
-        // return back();
+        Alert::success('Sucessfully Activate')->persistent('Dismiss');
+        return back();
     }
 
-    public function getRawMaterialsProducts(Request $request)
-    {
-        $rawMaterial = RawMaterial::with('product_raw_materials.products')->findOrFail($request->id);
+    // public function getRawMaterialsProducts(Request $request)
+    // {
+    //     $rawMaterial = RawMaterial::with('product_raw_materials.products')->findOrFail($request->id);
         
-        if ($rawMaterial->product_raw_materials != null)
-        {
-            return response()->json([
-                'status' => 1,
-                'products' => $rawMaterial->product_raw_materials->products->product_origin,
-                'percentage' => $rawMaterial->product_raw_materials->percent
-            ]);
-        }
-    }
+    //     if ($rawMaterial->product_raw_materials != null)
+    //     {
+    //         return response()->json([
+    //             'status' => 1,
+    //             'products' => $rawMaterial->product_raw_materials->products->product_origin,
+    //             'percentage' => $rawMaterial->product_raw_materials->percent
+    //         ]);
+    //     }
+    // }
 }
