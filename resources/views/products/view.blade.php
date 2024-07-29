7e68f709df4d33f504d867542a2974d913f8c8b9
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-07-29 18:42:47 +0800

    done

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -31,7 +31,7 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label"><b>DDW Number:</b></label>
                     <label class="col-sm-3 col-form-label">{{ $data->ddw_number }}</label>
-                    <label class="offset-sm-2 col-sm-2 col-form-label"><b>Raw Materials:</b></label>
+                    <label class="offset-sm-2 col-sm-2 col-form-label"><b>Raw Materials Cost:</b></label>
                     <label class="col-sm-2 col-form-label"><strong>USD</strong> {{number_format($rmc, 2)}}</label>
                 </div>
                 <div class="form-group row">
@@ -171,7 +171,7 @@
 
                     @include('products.edit_all_product_specification')
                     <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-hover" id="specification_table" width="100%">
+                        <table class="table table-striped table-bordered table-hover tables" id="specification_table" width="100%">
                             <thead>
                                 <tr>
                                     <th>Parameter</th>
@@ -212,7 +212,7 @@
                     @include('products.add_pds')
                     
                     <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-hover" id="specification_table" width="100%">
+                        <table class="table table-striped table-bordered table-hover tables" id="specification_table" width="100%">
                             <thead>
                                 <tr>
                                     <th>Product</th>
@@ -254,7 +254,7 @@
                     @include('products.add_file')
                     @include('products.edit_all_product_files')
                     <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-hover" id="specification_table" width="100%">
+                        <table class="table table-striped table-bordered table-hover tables" id="specification_table" width="100%">
                             <thead>
                                 <tr>
                                     <th>Name</th>
@@ -311,7 +311,7 @@
                 </div>
                 <div class="tab-pane fade " id="rmc" role="tabpanel" aria-labelledby="rmc-tab">
                     <div class="table-responsive">
-                        <table class="table table-hover table-bordered table-striped">
+                        <table class="table table-hover table-bordered table-striped tables" width="100%">
                             <thead>
                                 <tr>
                                     <th>Effective Date</th>
@@ -319,16 +319,16 @@
                                     <th>RMC (EUR)</th>
                                     <th>RMC (PHP)</th>
                                 </tr>
-                                <tbody>
-                                </tbody>
                             </thead>
+                            <tbody>
+                            </tbody>
                         </table>
                     </div>
                 </div>
                 <div class="tab-pane fade" id="client" role="tabpanel" aria-labelledby="client-tab">...</div>
                 <div class="tab-pane fade active show" id="identical" role="tabpanel" aria-labelledby="identical-tab">
                     <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-hover">
+                        <table class="table table-striped table-bordered table-hover tables">
                             <thead>
                                 <tr>
                                     <th>DDW Number</th>
@@ -339,7 +339,6 @@
                             </thead>
                             <tbody>
                                 @foreach ($identicalComposition as $ic)
-                                    {{-- {{dd($ic->products)}} --}}
                                     <tr>
                                         <td>{{$ic->products->ddw_number}}</td>
                                         <td>
@@ -369,7 +368,7 @@
                 </div>
                 <div class="tab-pane fade" id="historicalLogs" role="tabpanel" aria-labelledby="identical-tab">
                     <div class="table-responsive">
-                        <table class="table table-hover table-striped table-bordered">
+                        <table class="table table-hover table-striped table-bordered tables" width="100%">
                             <thead>
                                 <tr>
                                     <th>Date</th>
@@ -378,19 +377,13 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @if(count($data->productEventLogs) > 0)
-                                    @foreach ($data->productEventLogs as $logs)
-                                        <tr>
-                                            <td>{{date('M d Y', strtotime($logs->TimeStamp))}}</td>
-                                            <td>{{$logs->userByUserId->full_name}}</td>
-                                            <td>{{$logs->Details}}</td>
-                                        </tr>
-                                    @endforeach
-                                @else
+                                @foreach ($data->productEventLogs as $logs)
                                     <tr>
-                                        <td colspan="3">No history available.</td>
+                                        <td>{{date('M d Y', strtotime($logs->TimeStamp))}}</td>
+                                        <td>{{$logs->userByUserId->full_name}}</td>
+                                        <td>{{$logs->Details}}</td>
                                     </tr>
-                                @endif
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -430,70 +423,22 @@
     }
 </style>
 
-{{-- <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap4.js"></script>
 <script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js"></script>
 <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.bootstrap4.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script> --}}
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
 
-{{-- <script>
-    $(document).ready(function(){
-        $(document).on('click', '.submit_approval', function(){
-            product_id = $(this).attr('id');
-            $('#confirmModal').modal('show');
-            $('.modal-title').text("Submit for Approval");
-        });
-
-        dataTableInstance = new DataTable('#material_table', {
-            destroy: true, // Destroy and re-initialize DataTable on each call
-            processing: true,
-            serverSide: true, // Ensure server-side processing is enabled
-            pageLength: 25,
-            layout: {
-                topStart: {
-                    buttons: [
-                        'copy',
-                        {
-                            extend: 'excel',
-                            text: 'Export to Excel',
-                            filename: 'Material', // Set the custom file name
-                            title: 'Material' // Set the custom title
-                        }
-                    ]
-                }
-            },
-            ajax: {
-                url: "{{ route('activities.index') }}"
-            },
-            columns: [
-                {
-                    data: 'ActivityNumber',
-                    name: 'ActivityNumber'
-                },
-                {
-                    data: 'ScheduleFrom',
-                    name: 'ScheduleFrom',
-                },
-                {
-                    data: 'action',
-                    name: 'action',
-                    orderable: false
-                }
-            ],
-            columnDefs: [
-                {
-                    targets: [0, 1], // Target the Title column
-                    render: function(data, type, row) {
-                        return '<div style="white-space: break-spaces; width: 100%;">' + data + '</div>';
-                    }
-                }
-            ]
-        });
-    });   
-</script> --}}
 <script>
     $(document).ready(function() {
+        
+        new DataTable('.tables', {
+            destroy: true,
+            processing: true,
+            pageLength: 10,
+            ordering: false
+        });
 
         $("#addBtn").on('click', function() {
             
