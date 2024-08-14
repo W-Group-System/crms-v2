47ad60eb30e6dafe3ac92a9ff532d2e940600c06
JunJihad <junjihadbarroga@gmail.com>
2024-08-14 17:10:30 +0800

    fix

--- resources/views/product_evaluations/view.blade.php
+++ resources/views/product_evaluations/view.blade.php
@@ -702,8 +702,7 @@
 @foreach ($rpeFileUploads as $fileupload)
 @include('product_evaluations.edit_files')
 @endforeach
-@include('product_evaluations.cancel')
-@include('product_evaluations.close')
+
 {{-- @include('sample_requests.upload_srf_file') --}}
 {{-- @include('sample_requests.create_raw_materials') --}}
 {{-- @foreach ($requestEvaluation as $srf)
