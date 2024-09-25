3d96026f166578d39270f33f1d5a95ca3c71682b
Mark Ian <bautistamarkian083@gmail.com>
2024-09-25 08:56:32 +0800

    validation for RND

--- resources/views/customer_requirements/accept_crr.blade.php
+++ resources/views/customer_requirements/accept_crr.blade.php
@@ -14,7 +14,7 @@
                         <div class="col-lg-12">
                             <div class="form-group">
                                 <label>Remarks</label>
-                                <textarea name="accept_remarks" class="form-control" cols="50" rows="10" placeholder="Enter remarks" required></textarea>
+                                <textarea name="accept_remarks" class="form-control" cols="50" rows="10" placeholder="Enter remarks"></textarea>
                             </div>
                         </div>
                     </div>
