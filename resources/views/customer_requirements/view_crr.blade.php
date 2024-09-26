c68edd94f14c3e42c9ef8784eb8a6dcdd7980c7c
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-09-26 14:57:31 +0800

    done

--- resources/views/customer_requirements/view_crr.blade.php
+++ resources/views/customer_requirements/view_crr.blade.php
@@ -547,28 +547,10 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-3 col-md-2"><p class="mb-0"><b>Recommendation : </b></p></div>
-                    <div class="col-sm-3 col-md-3">
-                        <p class="mb-0">
-                            @php
-                            $crr_recommendation = $crr->Recommendation;
-                            $pattern = '/\[(.*?)\]/';
-                        
-                            $crr_linked = preg_replace_callback($pattern, function($matches) {
-                                $code = $matches[1];
-                                $productId = getProductIdByCode($code);
-                                
-                                if ($productId != null) {
-                                    return '<a href="'.url('view_product/'.$productId).'">'.$matches[0].'</a>';
-                                }
-                                return $matches[0];
-                            }, $crr_recommendation);
-                            @endphp
-                            {!! nl2br($crr_linked ) !!}
-                        </p>
-                    </div>
+                    <div class="col-sm-3 col-md-2"></div>
+                    <div class="col-sm-3 col-md-3"></div>
                     <div class="col-sm-3 col-md-2"><p class="mb-0"><b>Date Completed :</b></p></div>
-                    <div class="col-sm-3 col-md-2">
+                    <div class="col-sm-3 col-md-3">
                         <p class="mb-0">
                             @if($crr->DateCompleted != null)
                             {{date('Y-m-d', strtotime($crr->DateCompleted))}}
@@ -578,11 +560,9 @@
                         </p>
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <div class="col-sm-3 col-md-2">
-                    </div>
-                    <div class="col-sm-3 col-md-3">
-                    </div>
+                <div class="row">
+                    <div class="col-sm-3 col-md-2"></div>
+                    <div class="col-sm-3 col-md-3"></div>
                     <div class="col-sm-3 col-md-2"><p class="mb-0"><b>Days Late : </b></p></div>
                     <div class="col-sm-3 col-md-3">
                         @php
@@ -601,6 +581,28 @@
                         @endphp
                         <p class="mb-0">
                             {{$days_late .' day' .$s}}
+                        </p>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-3 col-md-2"><p class="mb-0"><b>Recommendation : </b></p></div>
+                    <div class="col-sm-3 col-md-3">
+                        <p class="mb-0">
+                            @php
+                            $crr_recommendation = $crr->Recommendation;
+                            $pattern = '/\[(.*?)\]/';
+                        
+                            $crr_linked = preg_replace_callback($pattern, function($matches) {
+                                $code = $matches[1];
+                                $productId = getProductIdByCode($code);
+                                
+                                if ($productId != null) {
+                                    return '<a href="'.url('view_product/'.$productId).'">'.$matches[0].'</a>';
+                                }
+                                return $matches[0];
+                            }, $crr_recommendation);
+                            @endphp
+                            {!! nl2br($crr_linked ) !!}
                         </p>
                     </div>
                 </div>
