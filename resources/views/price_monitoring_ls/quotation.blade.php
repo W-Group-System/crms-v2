34264aa0aa614be0554bf2f3bee0c8feecfc0303
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-10-09 14:58:04 +0800

    done

--- resources/views/price_monitoring_ls/quotation.blade.php
+++ resources/views/price_monitoring_ls/quotation.blade.php
@@ -260,6 +260,13 @@
                 <p class="right-p">We are looking forward to your favorable feedback.</p>
                 <p class="right-p">Thank you for your time and cosideration!</p>
                 <p class="right-p">Sincerely,</p>
+                @foreach ($price_monitoring_ls->requestPriceProducts as $price_products)
+                    @if($price_products->LsalesMarkupPercent >= 15)
+                        <p class="right-p">{{auth()->user()->full_name}}</p>
+                    @else
+                        <p class="right-p">Maricris J. Ribon</p>
+                    @endif
+                @endforeach
                 <br>
                 <br>
                 {{-- {{dd($price_monitoring_ls->requestPriceProducts)}} --}}
