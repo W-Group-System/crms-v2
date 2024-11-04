effdafd23b1bae480a370e3a79a9d920e21f959d
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-11-04 16:31:56 +0800

    done ui

--- resources/views/products/new.blade.php
+++ resources/views/products/new.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.header')
 @section('content')
 <div class="col-lg-12 grid-margin stretch-card">
-    <div class="card">
+    <div class="card rounded-0 border border-primary border-1">
+        <div class="card-header rounded-0 bg-primary font-weight-bold text-white">
+            <p class="m-0">List of Products (New)</p>
+        </div>
         <div class="card-body">
-            <h4 class="card-title d-flex justify-content-between align-items-center">Product List (New)</h4>
+            {{-- <h4 class="card-title d-flex justify-content-between align-items-center">Product List (New)</h4> --}}
 
             <div class="mb-3">
                 <button class="btn btn-md btn-outline-info">Copy</button>
