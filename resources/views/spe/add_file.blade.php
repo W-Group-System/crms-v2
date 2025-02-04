6a61ba70ed3eaef96918be73b3b3bfec90aaef5e
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-02-04 14:59:18 +0800

    done

--- resources/views/spe/add_file.blade.php
+++ resources/views/spe/add_file.blade.php
@@ -11,12 +11,12 @@
             <div class="modal-body">
                 <form method="POST" id="form_spe_file" enctype="multipart/form-data" action="{{ url('uploadSpeFiles') }}">
                     @csrf
-                    <div class="spe_file">
+                    <div class="spe_file" id="spe_file">
                         <div class="row">
-                            <div class="col-lg-12 mb-3">
+                            {{-- <div class="col-lg-12 mb-3">
                                 <!-- <label for="name">Name</label> -->
                                 <input type="hidden" name="name[]" class="form-control" id="name" placeholder="Enter Name">
-                            </div>
+                            </div> --}}
                             <!-- @if(authCheckIfItsRnd(auth()->user()->department_id))
                                 <div class="col-lg-12">
                                     <div class="form-group">
@@ -34,18 +34,40 @@
                                 </div>
                             @endif -->
                             <div class="col-lg-12 mb-3">
+                                <label for="name">Name</label>
+                                <input type="text" name="name[]" class="form-control fileNames" id="fileNames" placeholder="">
+                            </div>
+                            <div class="col-lg-12 mb-3">
+                                <div class="form-group">
+                                    <div class="group-confidential">
+                                        <label>Is Confidential :</label>
+                                        <input type="checkbox" name="is_confidential[]" value="1" onclick="toggleConfidential()">
+                                        <input type="hidden" name="is_confidential[]" value="0">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 mb-3">
+                                <div class="form-group">
+                                    <div class="group-review">
+                                        <label>Is For Review :</label>
+                                        <input type="checkbox" name="is_for_review[]" value="1" onclick="toggleReview()">
+                                        <input type="hidden" name="is_for_review[]" value="0">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 mb-3">
                                 <label for="spe_file">Browse Files</label>
-                                <input type="file" class="form-control" id="spe_file" name="spe_file[]">
+                                <input type="file" class="form-control" id="spe_file" name="spe_file[]" onchange="uploadFiles()">
                             </div>
                             <div class="col-lg-12">
                                 <input type="hidden" class="form-control" name="spe_id" value="{{ $data->id }}">
                             </div>
-                            <div class="col-lg-12">
-                                <button type="button" class="btn btn-sm btn-primary addSpeFile"><i class="ti-plus"></i></button>
-                                <button type="button" class="btn btn-sm btn-danger deleteRowBtn" hidden><i class="ti-trash"></i></button>
-                            </div>
                         </div>
                     </div> 
+                    <div id="speButton">
+                        <button type="button" class="btn btn-sm btn-primary" onclick="addSpeFile()"><i class="ti-plus"></i></button>
+                        <button type="button" class="btn btn-sm btn-danger" onclick="deleteSpeFile()"><i class="ti-trash"></i></button>
+                    </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         <button type="submit" value="Receive" class="btn btn-success">Save</button>
@@ -57,6 +79,105 @@
 </div>
 
 <script>
+    function addSpeFile()
+    {
+        const newRow = document.createElement("div")
+        newRow.classList.add('row')
+        newRow.innerHTML = `
+            <div class="col-lg-12 mb-3">
+                <label for="name">Name</label>
+                <input type="text" name="name[]" class="form-control fileNames" id="fileNames" placeholder="">
+            </div>
+            <div class="col-lg-12 mb-3">
+                <div class="form-group">
+                    <div class="group-confidential">
+                        <label>Is Confidential :</label>
+                        <input type="checkbox" name="is_confidential[]" value="1" onclick="toggleConfidential()">
+                        <input type="hidden" name="is_confidential[]" value="0">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 mb-3">
+                <div class="form-group">
+                    <div class="group-review">
+                        <label>Is For Review :</label>
+                        <input type="checkbox" name="is_for_review[]" value="1" onclick="toggleReview()">
+                        <input type="hidden" name="is_for_review[]" value="0">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 mb-3">
+                <label for="spe_file">Browse Files</label>
+                <input type="file" class="form-control" id="spe_file" name="spe_file[]" onchange="uploadFiles()">
+            </div>
+            <div class="col-lg-12">
+                <input type="hidden" class="form-control" name="spe_id" value="{{ $data->id }}">
+            </div>
+        `
+
+        document.getElementById('spe_file').append(newRow)
+    }
+
+    function deleteSpeFile()
+    {
+        const lastElement = document.getElementById('spe_file')
+        const lastChildElement = lastElement.lastChild
+
+        if (lastElement.children.length > 1)
+        {
+            lastElement.removeChild(lastChildElement)
+        }
+    }
+
+    function uploadFiles()
+    {
+        const name = event.target.files[0].name
+        const fileNames = event.target.closest('.row').querySelector('.fileNames');
+        fileNames.value = name
+    }
+
+    function toggleConfidential()
+    {
+        var closestRow = event.target.closest('.row')
+        var confidentialInput = closestRow.querySelector('input[type="checkbox"][name="is_confidential[]"]')
+        
+        if (confidentialInput.checked)
+        {
+            var hiddenInput = closestRow.querySelector('input[type="hidden"][name="is_confidential[]"]').remove()
+        }
+        else
+        {
+            var hiddenInput = document.createElement('input')
+            hiddenInput.type = 'hidden'
+            hiddenInput.name = 'is_confidential[]'
+            hiddenInput.value = 0
+
+            closestRow.querySelector('.group-confidential').appendChild(hiddenInput)
+        }
+    }
+
+    function toggleReview()
+    {
+        var closestRow = event.target.closest('.row')
+        var confidentialInput = closestRow.querySelector('input[type="checkbox"][name="is_for_review[]"]')
+        
+        if (confidentialInput.checked)
+        {
+            var hiddenInput = closestRow.querySelector('input[type="hidden"][name="is_for_review[]"]').remove()
+        }
+        else
+        {
+            var hiddenInput = document.createElement('input')
+            hiddenInput.type = 'hidden'
+            hiddenInput.name = 'is_for_review[]'
+            hiddenInput.value = 0
+
+            closestRow.querySelector('.group-review').appendChild(hiddenInput)
+        }
+    }
+
+</script>
+{{-- <script>
     $(document).ready(function() {
         function addSseForm() {
             const rowIndex = $('.spe_file').length; // Get the current number of rows to set a unique index
@@ -116,7 +237,7 @@
             $('.spe_file').find('.deleteRowBtn').hide();
         }
     });
-</script>
+</script> --}}
 
 <!-- @if(authCheckIfItsRnd(auth()->user()->department_id))
     <div class="col-lg-12">
