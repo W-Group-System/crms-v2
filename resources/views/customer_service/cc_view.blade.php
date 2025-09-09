cafab6a33215a64e5ffe016dc45e5d76e262d8f0
Mark Ian <bautistamarkian083@gmail.com>
2025-09-09 11:15:48 +0800

    print button not show

--- resources/views/customer_service/cc_view.blade.php
+++ resources/views/customer_service/cc_view.blade.php
@@ -94,7 +94,7 @@
                             </button>
                         @endif
                     @endif -->
-                    @if($data->Progress == 60 && $data->Progress == 70)
+                    @if($data->Progress == 60 || $data->Progress == 70)
                         <a class="btn btn-outline-danger btn-icon-text" href="{{url('print_cc/'.$data->id)}}" target="_blank">
                             <i class="ti ti-printer btn-icon-prepend"></i>
                             Print
