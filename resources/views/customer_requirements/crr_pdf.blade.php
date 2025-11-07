cde1f2bf1381541ca55bddb6a1c6f15ac8bd53ae
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-11-07 15:07:43 +0800

    done

--- resources/views/customer_requirements/crr_pdf.blade.php
+++ resources/views/customer_requirements/crr_pdf.blade.php
@@ -47,7 +47,8 @@ p {
 }
 
 header {
-    margin-top: -75px;
+    margin-top: -75pxs;
+    margin-bottom: 0;
 }
 
 input[type="checkbox"] {
@@ -75,182 +76,178 @@ input[type="checkbox"] {
         </table>
     </header>
 
-    <div class="page-break">
-        <table border="1" style="width: 100%; font-size:9" cellspacing="0" cellpadding="0">
-            
-            <table border="1" style="width: 80%; font-size:9; margin: 0 auto;" class="mt-4" cellspacing="0" cellpadding="0">
-                <h4 class="text-center">{{$crr->client->Name}}</h4>
-            </table>
-
-            <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td width="15%">
-                        <p class="text-right">Request Date </p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{date('F d, Y', strtotime($crr->DateCreated))}}</strong></p>
-                    </td>
-                    <td width="25%">
-                        <p class="text-right font-weight-bold">Crr No: </p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->CrrNumber}}</strong></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="15%">
-                        <p class="text-right">Deadline </p>
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{date('F d, Y', strtotime($crr->DueDate))}}</strong></p>
-                    </td>
-                    <td width="25%">
-                        <p class="text-right"><i><small>Consolidate with</small></i></p>
-                    </td>
-                    <td width="25%">
-                        <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td>
-                                    <p class="ml-4 mb-0"><strong>RPE No.:</strong></p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <p class="ml-4 mb-0"><strong>CRR No.:</strong></p>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="15%">
-                        <p class="text-right">Application</p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->product_application->Name}}</strong></p>
-                    </td>
-                    <td width="25%">
-                        <p class="text-right">Target Price: </p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->TargetPrice}}</strong></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="15%">
-                        <p class="text-right">Country</p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->client->clientcountry->Name}}</strong></p>
-                    </td>
-                    <td width="25%">
-                        <p class="text-right">Competitor: </p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">@if($crr->Competitor){{$crr->Competitor}}@else &nbsp; @endif</strong></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="15%">
-                        <p class="text-right">Region</p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->client->clientregion->Name}}</strong></p>
-                    </td>
-                    <td width="25%">
-                        <p class="text-right">Attachment: </p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">&nbsp;</strong></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="15%">
-                        <p class="text-right">Potential Volume</p> 
-                    </td>
-                    <td width="25%">
-                        <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->PotentialVolume}}</strong></p>
-                    </td>
-                    <td width="25%">
-                        {{-- <p class="text-right">Target Price: </p>  --}}
-                    </td>
-                    <td width="25%">
-                        {{-- <p class="ml-1"><strong style="border-bottom: 1px dashed black;">{{$crr->TargetPrice}}</strong></p> --}}
-                    </td>
-                </tr>
-            </table>
-            <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td colspan="4" class="bg-secondary">
-                        <p class="text-center font-weight-bold">Details of the Requirement</p>
-                    </td>
-                </tr>
-            </table>
-            <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td colspan="5">
-                        <p class="ml-5">{!! nl2br($crr->DetailsOfRequirement) !!}</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-top: 50;">
-                        <input type="checkbox"> <small>Data Sheet</small>
-                    </td>
-                    <td style="padding-top: 50;">
-                        <input type="checkbox"> <small>Nutritional Data</small>
-                    </td>
-                    <td style="padding-top: 50;">
-                        <input type="checkbox"> <small>Formula</small>
-                    </td>
-                    <td style="padding-top: 50;">
-                        <input type="checkbox"> <small>Code</small>
-                    </td>
-                    <td style="padding-top: 50;">
-                        <input type="checkbox"> <small>Others</small>
-                    </td>
-                </tr>
-            </table>
-            <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td colspan="4" class="bg-secondary">
-                        <p class="text-center font-weight-bold">Remarks</p>
-                    </td>
-                </tr>
-            </table>
-            <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td colspan="5" height="200" valign="top">
-                        {{-- <p>{!! nl2br($crr->DetailsOfRequirement) !!}</p> --}}
-                        <p class="ml-5 mb-0 mt-0">Recommended by: _________________</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="5">
-                        <p style="width: 100%; border-bottom:10px solid black;">&nbsp;</p>
-                    </td>
-                </tr>
-            </table>
-            <table border="1" style="width: 100%; font-size:9; margin-top:0;" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td width="33.33%">
-                        <p class="ml-3 mb-0 mt-0"><i><small>Prepared by</small></i></p>
-
-                        <p class="text-center" style="border-top: 1px solid black; width:80%; margin: 40px auto 5px auto;"><small>(Signature Over Printed Name)</small></p>
-                    </td>
-                    <td width="33.33%">
-                        <p class="ml-3 mb-0 mt-0"><i><small>Approved by</small></i></p>
-
-                        <p class="text-center" style="border-top: 1px solid black; width:80%; margin: 40px auto 5px auto;"><small>(Signature Over Printed Name)</small></p>
-                    </td>
-                    <td width="33.33%">
-                        <p class="ml-3 mb-0 mt-0"><i><small>Date Received by</small></i></p>
-
-                        <p class="text-center" style="border-top: 1px solid black; width:80%; margin: 40px auto 5px auto;"><small>(Signature Over Printed Name)</small></p>
-                    </td>
-                </tr>
-            </table>
+    {{-- <div class="page-break">
+    </div> --}}
+    <table border="1" style="width: 100%; font-size:9 margin-top:0; height:min-content;" cellspacing="0" cellpadding="0">
+        <table border="1" style="width: 80%; font-size:9; margin: 0 auto;" class="mt-4" cellspacing="0" cellpadding="0">
+            <h4 class="text-center">{{$crr->client->Name}}</h4>
         </table>
-    </div>
+        <table border="0" style="width: 100%; font-size:9; height:min-content; padding:5;" cellspacing="0" cellpadding="0">
+            <tr>
+                <td width="15%">
+                    <p class="text-right">Request Date </p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{date('F d, Y', strtotime($crr->DateCreated))}}</strong></p>
+                </td>
+                <td width="25%">
+                    <p class="text-right font-weight-bold">Crr No: </p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->CrrNumber}}</strong></p>
+                </td>
+            </tr>
+            <tr>
+                <td width="15%">
+                    <p class="text-right">Deadline </p>
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{date('F d, Y', strtotime($crr->DueDate))}}</strong></p>
+                </td>
+                <td width="25%">
+                    <p class="text-right"><i><small>Consolidate with</small></i></p>
+                </td>
+                <td width="25%">
+                    <table border="0" style="width: 100%; font-size:9;" cellspacing="0" cellpadding="0">
+                        <tr>
+                            <td>
+                                <p class="ml-4 mb-0"><strong>RPE No.:</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p class="ml-4 mb-0"><strong>CRR No.:</strong></p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td width="15%">
+                    <p class="text-right">Application</p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->product_application->Name}}</strong></p>
+                </td>
+                <td width="25%">
+                    <p class="text-right">Target Price: </p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->TargetPrice}}</strong></p>
+                </td>
+            </tr>
+            <tr>
+                <td width="15%">
+                    <p class="text-right">Country</p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->client->clientcountry->Name}}</strong></p>
+                </td>
+                <td width="25%">
+                    <p class="text-right">Competitor: </p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">@if($crr->Competitor){{$crr->Competitor}}@else &nbsp; @endif</strong></p>
+                </td>
+            </tr>
+            <tr>
+                <td width="15%">
+                    <p class="text-right">Region</p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->client->clientregion->Name}}</strong></p>
+                </td>
+                <td width="25%">
+                    <p class="text-right">Attachment: </p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">&nbsp;</strong></p>
+                </td>
+            </tr>
+            <tr>
+                <td width="15%">
+                    <p class="text-right">Potential Volume</p> 
+                </td>
+                <td width="25%">
+                    <p class="ml-1 text-center"><strong class="d-inline-block" style="border-bottom: 1px dashed black; width:100%;">{{$crr->PotentialVolume}}</strong></p>
+                </td>
+                <td width="25%">
+                    {{-- <p class="text-right">Target Price: </p>  --}}
+                </td>
+                <td width="25%">
+                    {{-- <p class="ml-1"><strong style="border-bottom: 1px dashed black;">{{$crr->TargetPrice}}</strong></p> --}}
+                </td>
+            </tr>
+        </table>
+    </table>
+    <table border="1" style="width: 100%; font-size:9; margin-top:0;" cellspacing="0" cellpadding="0">
+        <tr>
+            <td colspan="4" class="bg-secondary">
+                <p class="text-center font-weight-bold">Details of the Requirement</p>
+            </td>
+        </tr>
+    </table>
+    <table border="1" style="width: 100%; font-size:9; margin-top:0; height:min-content;" cellspacing="0" cellpadding="10">
+        <tr>
+            <td colspan="5">
+                <p style="width:100vw;">{!! nl2br($crr->DetailsOfRequirement) !!}</p>
+            </td>
+        </tr>
+        <tr>
+            <td style="border: none;">
+                <input type="checkbox"> <small>Data Sheet</small>
+            </td>
+            <td style="border: none;">
+                <input type="checkbox"> <small>Nutritional Data</small>
+            </td>
+            <td style="border: none;">
+                <input type="checkbox"> <small>Formula</small>
+            </td>
+            <td style="border: none;">
+            </td>
+            <td style="border: none;">
+                <input type="checkbox"> <small>Others</small>
+            </td>
+        </tr>
+    </table>
+    <table border="1" style="width: 100%; font-size:9; margin-top:0;" cellspacing="0" cellpadding="0">
+        <tr>
+            <td colspan="4" class="bg-secondary">
+                <p class="text-center font-weight-bold">Remarks</p>
+            </td>
+        </tr>
+    </table>
+    <table border="1" style="width: 100%; font-size:9; height:min-content; margin-top:0;" cellspacing="0" cellpadding="0">
+        <tr>
+            <td colspan="5" height="200" valign="top">
+                <p class="ml-5 mb-0 mt-0">Recommended by: _________________</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="5">
+                <p style="width: 100%; border-bottom:10px solid black;"></p>
+            </td>
+        </tr>
+    </table>
+    <table border="1" style="width: 100%; font-size:9; margin-top:0;" cellspacing="0" cellpadding="0">
+        <tr>
+            <td width="33.33%">
+                <p class="ml-3 mb-0 mt-0"><i><small>Prepared by</small></i></p>
+
+                <p class="text-center" style="border-top: 1px solid black; width:80%; margin: 40px auto 5px auto;"><small>(Signature Over Printed Name)</small></p>
+            </td>
+            <td width="33.33%">
+                <p class="ml-3 mb-0 mt-0"><i><small>Approved by</small></i></p>
+
+                <p class="text-center" style="border-top: 1px solid black; width:80%; margin: 40px auto 5px auto;"><small>(Signature Over Printed Name)</small></p>
+            </td>
+            <td width="33.33%">
+                <p class="ml-3 mb-0 mt-0"><i><small>Date Received by</small></i></p>
+
+                <p class="text-center" style="border-top: 1px solid black; width:80%; margin: 40px auto 5px auto;"><small>(Signature Over Printed Name)</small></p>
+            </td>
+        </tr>
+    </table>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
