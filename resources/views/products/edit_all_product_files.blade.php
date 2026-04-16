64ee998ab1c24441e8fd85ae00bb40688a1169e2
Emman0304 <108043649+Emman0304@users.noreply.github.com>
2026-04-16 18:12:50 +0800

    Revert "Feature/files"

--- resources/views/products/edit_all_product_files.blade.php
+++ resources/views/products/edit_all_product_files.blade.php
@@ -12,7 +12,48 @@
                 <input type="hidden" name="product_id" value="{{$data->id}}">
                 <div class="modal-body" style="padding: 20px">
                     <div class="product_files_container ">
-                        
+                        @foreach ($data->productFiles as $pf)
+                            <div class="row">
+                                <div class="col-lg-10">
+                                    <fieldset class="border border-primary p-3 mb-3">
+                                        <div class="row">
+                                                <input type="hidden" name="file_id[]" class="form-control form-control-sm" value="{{$pf->Id}}" required>
+                                            <div class="col-lg-6">
+                                                <label>Name :</label>
+                                                <input type="text" name="name[]" class="form-control form-control-sm" value="{{$pf->Name}}" required>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <label>Client :</label>
+                                                <select name="client[]" class="js-example-basic-single form-control form-control-sm">
+                                                    <option value="">-Client-</option>
+                                                    @foreach ($client as $c)
+                                                        <option value="{{$c->id}}" @if($c->id == $pf->ClientId) selected @endif>{{$c->Name}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <label>Description :</label>
+                                                {{-- <input type="text" name="description[]" class="form-control form-control-sm" value="{{$pf->Description}}" required>  --}}
+                                                <textarea name="description[]" class="form-control" cols="30" rows="10">{{$pf->Description}}</textarea>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <label>Is Confidential :</label>
+                                                <input type="checkbox" name="is_confidential[]" @if($pf->IsConfidential == 1) checked @endif> 
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <label>File :</label>
+                                                <input type="file" name="productFiles[]" id="file" value="{{ $pf->Path }}" class="form-control form-control-sm">
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                                <div class="col-lg-2">
+                                    <button class="btn btn-sm btn-danger mb-3 removeBtnFiles" type="button" >
+                                        <i class="ti-minus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                     <button class="btn btn-sm btn-success mb-3 addBtnFiles" type="button">
                         <i class="ti-plus"></i>
