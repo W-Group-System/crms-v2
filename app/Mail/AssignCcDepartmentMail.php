9ca8727780a6479f7f8f4e6f38d9c7a25de12d08
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-04-29 14:34:15 +0800

    Initial Update

--- app/Mail/AssignCcDepartmentMail.php
+++ app/Mail/AssignCcDepartmentMail.php
@@ -51,7 +51,7 @@ class AssignCcDepartmentMail extends Mailable
         if (!empty($this->cc_attachments))
         {
             foreach ($this->cc_attachments as $cc_attachment) {
-                $filePath = public_path('storage/'.$cc_attachment);
+                $filePath = public_path('storage/'.$cc_attachment->Path);
                 $email->attach($filePath);
             }
         }
