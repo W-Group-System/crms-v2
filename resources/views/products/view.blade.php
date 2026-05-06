0d90161384534a7e6f21d2698f9dfbf77e81be25
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-05-06 17:26:47 +0800

    u

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -29,7 +29,7 @@
                 $rmc = rmc($data->productMaterialComposition, $data->id);
                 $identicalComposition = identicalComposition($data->productMaterialComposition, $data->id);
                 $customerRequirements = customerRequirements($data->code);
-                dd($customerRequirements);
+                // dd($customerRequirements);
                 $productRps = productRps($data->code);
                 $history_rmc = historyRmc($data->productMaterialComposition, $data->id);
             @endphp
