97a87a6cdd5a66ed930fc3dcd9dad87a97e979fb
JunJihad <junjihadbarroga@gmail.com>
2026-08-25 13:43:05 +0800

    done

--- app/Exports/CustomerComplaintExport.php
+++ app/Exports/CustomerComplaintExport.php
@@ -113,7 +113,7 @@ class CustomerComplaintExport implements FromCollection, WithHeadings, WithMappi
                     $row->created_at,
                     $row->CompanyName,
                     $row->ContactName,
-                    $row->concernedDept->Name ?? 'N/A',
+                    $row->concernedDept->Name ?? '',
                     $row->CustomerRemarks,
                     optional($row->users)->full_name,
                     $status,
