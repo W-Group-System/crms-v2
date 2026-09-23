77b669eb6546774413f014a24dbab98024308488
JunJihad <junjihadbarroga@gmail.com>
2026-09-23 14:59:52 +0800

    apply only to rnd

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -369,52 +369,55 @@
                             <div class="collapse" id="tables">
                                 <ul class="nav flex-column sub-menu">
                                     @if(viewModule('Customer Requirement', $department, $role) == "yes")
-                                    {{-- <li class="nav-item"><a class="nav-link" href="{{ url('/customer_requirement?open=10') }}">Customer Requirement</a></li>  --}}
-                                    <li class="nav-item customer-requirement-parent">
+                                        @if(Auth::check() && optional(Auth::user()->role)->type == 'RND')
+                                            <li class="nav-item customer-requirement-parent">
 
-                                        <a class="nav-link"
-                                            href="javascript:void(0);"
-                                            onclick="toggleCustomerRequirement(event)">
-                                            <span>Customer Requirement</span>
-                                            <i class="menu-arrow"></i>
-                                        </a>
+                                            <a class="nav-link"
+                                                href="javascript:void(0);"
+                                                onclick="toggleCustomerRequirement(event)">
+                                                <span>Customer Requirement</span>
+                                                <i class="menu-arrow"></i>
+                                            </a>
 
-                                        <div id="customerRequirement" class="customer-requirement-menu">
-                                            <ul class="customer-requirement-list">
+                                            <div id="customerRequirement" class="customer-requirement-menu">
+                                                <ul class="customer-requirement-list">
 
-                                                <li>
-                                                    <a href="{{ url('/customer_requirement?nature=all') }}">
-                                                        All
-                                                    </a>
-                                                </li>
+                                                    <li>
+                                                        <a href="{{ url('/customer_requirement?nature=all') }}">
+                                                            All
+                                                        </a>
+                                                    </li>
 
-                                                <li>
-                                                    <a href="{{ url('/customer_requirement?nature=recommendation') }}">
-                                                        Product Recommendation
-                                                    </a>
-                                                </li>
+                                                    <li>
+                                                        <a href="{{ url('/customer_requirement?nature=recommendation') }}">
+                                                            Product Recommendation
+                                                        </a>
+                                                    </li>
 
-                                                <li>
-                                                    <a href="{{ url('/customer_requirement?nature=documentation') }}">
-                                                        Documentation Requests
-                                                    </a>
-                                                </li>
+                                                    <li>
+                                                        <a href="{{ url('/customer_requirement?nature=documentation') }}">
+                                                            Documentation Requests
+                                                        </a>
+                                                    </li>
 
-                                                <li>
-                                                    <a href="{{ url('/customer_requirement?nature=questionnaires') }}">
-                                                        Client Questionnaires
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="{{ url('/customer_requirement?nature=coding') }}">
-                                                        Product Coding
-                                                    </a>
-                                                </li>
+                                                    <li>
+                                                        <a href="{{ url('/customer_requirement?nature=questionnaires') }}">
+                                                            Client Questionnaires
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="{{ url('/customer_requirement?nature=coding') }}">
+                                                            Product Coding
+                                                        </a>
+                                                    </li>
 
-                                            </ul>
-                                        </div>
+                                                </ul>
+                                            </div>
 
-                                    </li>
+                                        </li>
+                                        @else
+                                             <li class="nav-item"><a class="nav-link" href="{{ url('/customer_requirement?open=10') }}">Customer Requirement</a></li> 
+                                        @endif
                                     @endif
                                     @if(viewModule('Request for Product Evaluation', $department, $role) == "yes")
                                     <li class="nav-item"><a class="nav-link" href="{{ url('/request_product_evaluation?open=10') }}">Request for Product Evaluation</a></li>
